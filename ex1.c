d77f5a6dfd7b53d4c97fda7b4574198a6b5934c6
Drinity <mattbutcher93@hotmail.com>
2016-10-17 17:33:44 +0100

    new stuff

--- ex1.c
+++ ex1.c
@@ -16,6 +16,7 @@
 #include <time.h>
 #include <math.h>
 #include <stdbool.h>
+#include "SOIL.h"
 
 #ifdef MACOSX
   #include <GLUT/glut.h>
@@ -25,7 +26,7 @@
 
 #define PI 3.14159
 #define STAR_NO 1000
-#define PART_NO 10000
+#define PART_NO 100000
 #define MASS_PART_NO 1
 #define G 1.0
 #define TIME 1.0
@@ -38,8 +39,10 @@ GLint AXIS_SIZE= 500;
 int axisEnabled= 1;
 int star_radius = 200000;
 int current_time = 0;
+bool pause = false;
 
 int left_right = 0;
+float lookdistance = 30000.0;
 
 GLfloat star_coods[STAR_NO][3];
 
@@ -77,16 +80,21 @@ void calcParticleValues() {
 	
 					//check if particle is too close to mass(is destoryed if so)
 					//approximated to a cube
-					if(((particles[i].x < (mass_paritcles[j].x + mass_paritcles[j].mass / 10)) && (particles[i].x > (mass_paritcles[j].x - mass_paritcles[j].mass / 10)))
-					&& ((particles[i].y < (mass_paritcles[j].y + mass_paritcles[j].mass / 10)) && (particles[i].y > (mass_paritcles[j].y - mass_paritcles[j].mass / 10)))
-					&& ((particles[i].z < (mass_paritcles[j].z + mass_paritcles[j].mass / 10)) && (particles[i].z > (mass_paritcles[j].z - mass_paritcles[j].mass / 10)))) {
+					if(((particles[i].x < (mass_paritcles[j].x + mass_paritcles[j].mass / 30)) && (particles[i].x > (mass_paritcles[j].x - mass_paritcles[j].mass / 30)))
+					&& ((particles[i].y < (mass_paritcles[j].y + mass_paritcles[j].mass / 30)) && (particles[i].y > (mass_paritcles[j].y - mass_paritcles[j].mass / 30)))
+					&& ((particles[i].z < (mass_paritcles[j].z + mass_paritcles[j].mass / 30)) && (particles[i].z > (mass_paritcles[j].z - mass_paritcles[j].mass / 30)))) {
 	
-						particles[i].mass = false;
+						particles[i].alive = false;
 					}
 				}
 	
 	
 				r = sqrt(pow(particles[i].u, 2) + pow(particles[i].v, 2) + pow(particles[i].w, 2));
+
+
+
+				particles[i].g = r / 100;
+				particles[i].b = r / 100;
 	
 			//calculate new velocity according to acceleration (force)
 			//some velocity is taken away to simulate orbit decay
@@ -113,7 +121,7 @@ void calcParticleAccel() {
 					diffY = mass_paritcles[j].y - particles[i].y;
 					diffZ = mass_paritcles[j].z - particles[i].z;
 	
-					r = sqrt(pow(diffX, 2) + pow(diffY, 2) + pow(diffZ, 2));
+					r = sqrt(pow(diffX, 2.0) + pow(diffY, 2.0) + pow(diffZ, 2.0));
 	
 					force = (G * mass_paritcles[j].mass * particles[i].mass) / r;
 	
@@ -134,19 +142,45 @@ void calcParticleAccel() {
 	}
 }
 
-void initParticles() {
-		int i;
-	for(i = 0; i < PART_NO; i++) {
-		particles[i].x = (myRand() * 5000 + 3000) * -1;
-		particles[i].y = (myRand() * 5000 + 1000);
-		particles[i].z = (myRand() * 5000 + 1000);
+void randPointOnSphere(float Cx, float Cy, float Cz, float r, float* x, float* y, float* z) {
+  float inclination;
+  float azimuth;
+  	
+  inclination = myRand() * 2 * PI;
+  azimuth = myRand() * 2 * PI;
 
-		particles[i].u = 60.0;
+   *x = r * sin(inclination) * cos(azimuth) + Cx; 
+   *y = r * sin(inclination) * sin(azimuth) + Cy;
+   *z = r * cos(inclination) + Cz;
+}
+
+void randPointInSphere(float Cx, float Cy, float Cz, float r, float* x, float* y, float* z) {
+  float inclination;
+  float azimuth;
+  	
+  inclination = myRand() * 2 * PI;
+  azimuth = myRand() * 2 * PI;
+
+   *x = r * myRand() * sin(inclination) * cos(azimuth) + Cx; 
+   *y = r * myRand() * sin(inclination) * sin(azimuth) + Cy;
+   *z = r * myRand() * cos(inclination) + Cz;
+}
+
+void initParticles() {
+	int i;
+	for(i = 0; i < PART_NO; i++) {
+		// particles[i].x = (myRand() * 5000 + 6000) * -1;
+		// particles[i].y = (myRand() * 5000 + 1000);
+		// particles[i].z = (myRand() * 5000 + 1000);
+
+		randPointInSphere(5000, 5000, 5000, 1000, &particles[i].x, &particles[i].y, &particles[i].z);
+
+		particles[i].u = 100.0;
 		particles[i].v = 2.0;
 		particles[i].w = 4.0;
 
-		particles[i].r = 0.0;
-		particles[i].g = 1.0;
+		particles[i].r = 1.0;
+		particles[i].g = 0.0;
 		particles[i].b = 0.0;
 
 		particles[i].Au = 0.0;
@@ -175,7 +209,7 @@ void initParticles() {
 		mass_paritcles[i].Av = 0.0;
 		mass_paritcles[i].Aw = 0.0;
 
-		mass_paritcles[i].mass = 2000.0;
+		mass_paritcles[i].mass = 15000.0;
 
 		particles[i].alive = false;
 	}
@@ -184,24 +218,23 @@ void initParticles() {
 void drawParticles() {
 
 	//draw particles
-	glPointSize(2.0f);
+	glPointSize(1.0f);
 	glBegin(GL_POINTS);
 	int i;
 
 	for(i = 0; i < PART_NO; i++) {
 		if(particles[i].alive) {
-			glColor3f(particles[i].r, particles[i].g, particles[i].b);
+			//glColor3f(particles[i].r, particles[i].g, particles[i].b);
 			glVertex3f(particles[i].x, particles[i].y, particles[i].z);
 		}
 	}
 	glEnd();
-
 	//draw massive particles
 	glPointSize(10.0f);
 	glBegin(GL_POINTS);
 
 	for(i = 0; i < MASS_PART_NO; i++) {
-		glColor3f(mass_paritcles[i].r, mass_paritcles[i].g, mass_paritcles[i].b);
+		//glColor3f(mass_paritcles[i].r, mass_paritcles[i].g, mass_paritcles[i].b);
 		glVertex3f(mass_paritcles[i].x, mass_paritcles[i].y, mass_paritcles[i].z);
 	}
 	glEnd();
@@ -228,7 +261,7 @@ void initStarCoords(void) {
 //taken from previous years lab
 void drawStarfield (void)
 {
-	glPointSize(1.0f);
+  glPointSize(1.0f);
   glBegin(GL_POINTS);
   glColor3f(1.0f, 1.0f, 1.0f);
   int i;
@@ -242,10 +275,12 @@ void drawStarfield (void)
 
 void animate(void)
 {
-		calcParticleValues();
-		calcParticleAccel();
+  if(!pause) {
+	calcParticleValues();
+	calcParticleAccel();
     glutPostRedisplay();
     current_time++;
+  }
 }
 
 ///////////////////////////////////////////////
@@ -253,19 +288,20 @@ void animate(void)
 void display()
 {
   glLoadIdentity();
-  gluLookAt(0.0, 0.0, 30000.0,
+  gluLookAt(0.0, 0.0, lookdistance,
             0.0, 0.0, 0.0,
             0.0, 1.0, 0.0);
   // Clear the screen
-  glClear(GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   // If enabled, draw coordinate axis
 
+  glMatrixMode(GL_MODELVIEW);
   glPushMatrix();
-  if(axisEnabled) glCallList(axisList);
-  drawStarfield();
-  drawParticles();
-	glRotatef(left_right,0.0,1.0,0.0);
-	glPopMatrix();
+    glRotatef(left_right,0.0,1.0,0.0);
+  	if(axisEnabled) glCallList(axisList);
+  	drawStarfield();
+  	drawParticles();
+  glPopMatrix();
 
   glutSwapBuffers();
 }
@@ -275,19 +311,15 @@ void display()
 void keyboard(unsigned char key, int x, int y)
 {
   if(key == 27) exit(0);
+  if(key == 'a') left_right = (left_right + 1) % 360;
+  if(key == 'd') left_right = (left_right - 1) % 360;
+  if(key == 'p') pause = !pause;
+  if(key == ' ') initParticles();
+  if(key == 'z') lookdistance += 500;
+  if(key == 'x') lookdistance -= 500;
   glutPostRedisplay();
 }
 
-void cursor_keys(int key, int x, int y) {
-  switch (key) {
-  case GLUT_KEY_LEFT:
-  	left_right = (left_right - 1) % 360;
-    break;
-  case GLUT_KEY_RIGHT:
-  	left_right = (left_right + 1) % 360;
-    break;
-  }
-}
 ///////////////////////////////////////////////
 
 void reshape(int width, int height)
@@ -327,11 +359,19 @@ void initGraphics(int argc, char *argv[])
   glutInit(&argc, argv);
   glutInitWindowSize(1600, 1200);
   glutInitWindowPosition(100, 100);
-  glutInitDisplayMode(GLUT_DOUBLE);
+
+  glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
   glutCreateWindow("COMP37111 Particles");
   glutDisplayFunc(display);
   glutKeyboardFunc(keyboard);
   glutReshapeFunc(reshape);
+
+  glEnable(GL_DEPTH_TEST);
+ 
+//  glEnable(GL_BLEND);
+// glDepthMask(GL_FALSE);
+// glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+
   makeAxes();
 }
 
@@ -342,13 +382,40 @@ int main(int argc, char *argv[])
   //double f;
   srand(time(NULL));
   initGraphics(argc, argv);
-
   initStarCoords();
   initParticles();
-  glutSpecialFunc(cursor_keys);
 
-  glEnable(GL_POINT_SMOOTH);
+  //glEnable(GL_POINT_SMOOTH);
+  glEnable(GL_POINT_SPRITE);
+
+  //Code from (14-Oct-16)
+  //taken from http://www.lonesock.net/soil.html
+  GLuint textureID = SOIL_load_OGL_texture
+  (
+	"fadedsprite.jpg",
+	SOIL_LOAD_AUTO,
+	SOIL_CREATE_NEW_ID,
+	SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
+  );
+
+  /* check for an error during the load process */
+  if( 0 == textureID )
+  {
+	printf( "SOIL loading error: '%s'\n", SOIL_last_result() );
+  }
+
+  //Code from (14-Oct-16)
+  //http://www.informit.com/articles/article.aspx?p=770639&seqNum=7
+  glBindTexture(GL_TEXTURE_2D, textureID);
+  glTexEnvi(GL_POINT_SPRITE, GL_COORD_REPLACE, GL_TRUE);
+glBegin(GL_POINTS);
+  glPointSize(100);
+glVertex3f(0, 1000, 0);
+glEnd();
+
+
   glutIdleFunc (animate);
+
   glutMainLoop();
 
   return 1;
